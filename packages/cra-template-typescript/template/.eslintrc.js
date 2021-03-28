79be0b5b3634300183a4be66b5dd9cb00b373c8f
Guillaume Mastio <gm3122@gmail.com>
2021-03-28 15:28:41 +0200

    eslintrc.js

---
+++ packages/cra-template-typescript/template/.eslintrc.js
@@ -0,0 +1,139 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'import',
+    'unused-imports',
+    'simple-import-sort',
+    'sort-keys-fix',
+    'sort-destructure-keys',
+    'jest',
+  ],
+  rules: {
+    'no-console': 'error',
+    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+    'react/display-name': 'off',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        shorthandFirst: true,
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react$', '^gatsby.*', '^netlify.*', '^@storybook.*', '^[a-z-]+'],
+          ['^@.*'],
+          ['^C/.*'],
+          ['^Q/.*'],
+          ['^>/.*'],
+          ['^~/.*'],
+          ['^T/.*'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'sort-imports': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    // enable the rule specifically for TypeScript files
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {},
+    },
+    // enable the rule specifically for TypeScript files
+    {
+      files: ['*.stories.ts', '*.stories.tsx'],
+      rules: {
+        'react/function-component-definition': 'off',
+      },
+    },
+    {
+      files: ['scripts/**'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['src/**'],
+      rules: {
+        'sort-keys-fix/sort-keys-fix': 'warn',
+        'sort-destructure-keys/sort-destructure-keys': [
+          2,
+          { caseSensitive: true },
+        ],
+      },
+    },
+    {
+      files: ['src/cms/config.js'],
+      rules: {
+        'sort-keys-fix/sort-keys-fix': 'off',
+      },
+    },
+  ],
+};
